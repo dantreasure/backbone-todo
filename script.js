7447776c5f7098bbdd0526c019d756c1c7889b6d
Daniel Treasure <dbtreasure@gmail.com>
2013-10-05 16:06:00 -0600

    completed inital backbone setup to add  an item to the todo list ul

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
+/*
+ * jQuery Setup
+ */
+
+
 $(document).ready(function() {
+
+    
+var newTodoView = new todoView(
+	{ 
+		model: new todo({
+			title: 'Test'
+		})
+		
+	}
+);
+
+newTodoView.render();
 });
+
+/*
+ * Backbone Setup
+ */
+
+var todo = Backbone.Model.extend({
+   defaults: {
+        title: '',
+        completed: false
+    }
+});
+
+var todoView = Backbone.View.extend({
+  el: '.todos-list',
+  template: _.template($('#todo-template').html()),
+  render: function() {
+    this.$el.append(this.template(this.model.toJSON()));
+  }
+});
+
